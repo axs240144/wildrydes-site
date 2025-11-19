4186ceb975b0a09aac22567745252ddb74e8bbfe
axs240144 <axs240144@utdallas.edu>
2025-11-18 19:50:00 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_BXIY3iCEh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2p7o01ssknhh8n4kkmnbgm8orf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3r0hvtphtfnle6avn4pr0qvsdo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
